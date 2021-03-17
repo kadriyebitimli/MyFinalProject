16312bb38adad33c72be3f1ba6e3966cc01ea8a0
bitim <bitimlikadriye393@gmail.com>
2021-03-17 20:17:23 +0300

    kodlama.io proje

---
+++ WebAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,34 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private ICategoryService _categoryservice;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryservice = categoryService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            var result = _categoryservice.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+
+        }
+    }
+}

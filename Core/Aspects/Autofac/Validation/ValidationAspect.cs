100adbfbc1417488fefea1ed130ce3e894521b85
bitim <bitimlikadriye393@gmail.com>
2021-03-24 20:37:06 +0300

    20 Mart

--- Core/Aspects/Autofac/Validation/ValidationAspect.cs
+++ Core/Aspects/Autofac/Validation/ValidationAspect.cs
@@ -17,7 +17,7 @@ namespace Core.Aspects.Autofac.Validation
             //defensive coding
             if (!typeof(IValidator).IsAssignableFrom(validatorType))
             {
-                throw new System.Exception("Bu bir doğrulama sınıfı değildir.");
+                throw new System.Exception("Bu bir doğrulama sınıfı değil");
             }
 
             _validatorType = validatorType;
@@ -33,6 +33,6 @@ namespace Core.Aspects.Autofac.Validation
             }
         }
 
-        
+
     }
 }
